c7b70136eb4103a205c776b5d4ebed2bf2df6e2f
Abdallah Sabry <sabryabdallah37@gmail.com>
2024-10-06 19:20:27 +0300

    test 1

---
+++ fns_and_dsa/arithmetic_operations.py
@@ -0,0 +1,16 @@
+def perform_operation(num1,num2,operation):
+    result = 0.0
+    match(operation):
+        case 'add':
+            result=num1+num2
+        case 'subtract':
+            result=num1-num2
+        case 'multiply':
+            result=num1*num2
+        case 'divide':
+            if num1 != 0:
+                result=num1*num2
+            else:
+                print("cant divide by zero")
+                return 
+    return result
