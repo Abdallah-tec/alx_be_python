56a0f88ae67e3374ebc4decbaa7a65b7320400bf
Abdallah Sabry <sabryabdallah37@gmail.com>
2024-10-13 21:44:23 +0300

    new

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -14,6 +14,6 @@ class BankAccount:
         else:
             return False
     def display_balance(self):
-        print(f"Current Balance: $['{self.account_Balance:.2f}']")
+        print(f"Current Balance: $[{self.account_Balance:.2f}]")
 
 
