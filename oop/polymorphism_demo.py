01ffe598d59c5cfe0c673923b22fd92d1ea33050
Abdallah Sabry <sabryabdallah37@gmail.com>
2024-10-20 02:21:47 +0300

    oop

---
+++ oop/polymorphism_demo.py
@@ -0,0 +1,25 @@
+import math
+
+class Shape:
+    def area(self):
+        pass
+
+class Rectangle(Shape):
+    length = 0
+    width = 0
+    def __init__(self,length, width):
+        self.length = length
+        self.width = width
+
+    def area(self):
+        return self.length * self.width
+
+class Circle(Shape):
+    radius = 0
+    def __init__(self,radius):
+        self.radius = radius
+        
+    def area(self):
+        return math.pi * self.radius * self.radius
+
+    
